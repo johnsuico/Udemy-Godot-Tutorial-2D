5eba001ac2fa0a60504146abbc8bcc881492122b
John Suico <johncdsuico@gmail.com>
2025-10-08 22:02:06 -0700

    Add jump animation

--- tappy-plane-tutorial/Scenes/Plane/Plane.gd
+++ tappy-plane-tutorial/Scenes/Plane/Plane.gd
@@ -3,6 +3,9 @@ extends CharacterBody2D
 # Getting the built in gravity value from project settings
 var _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
 
+# Get the animation player. Using it for the jump animation
+@onready var animation_player: AnimationPlayer = $AnimationPlayer
+
 # Constant for jump velocity
 const JUMP_POWER: float = -350.0
 
@@ -21,6 +24,8 @@ func _physics_process(delta: float) -> void:
 	
 	if Input.is_action_just_pressed("jump"):
 		velocity.y = JUMP_POWER # Change velocity when space is pressed to make the plane jump
+		animation_player.play("Jump")
+		
 	
 	# This moves the characterbody2d node using the velocity
 	move_and_slide()
