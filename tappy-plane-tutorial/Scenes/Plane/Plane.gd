9b0599f66f54ba752abfb9e1ba6a9c0f18005aea
John Suico <johncdsuico@gmail.com>
2025-10-08 21:45:15 -0700

    Add plane jump mechanic

--- tappy-plane-tutorial/Scenes/Plane/Plane.gd
+++ tappy-plane-tutorial/Scenes/Plane/Plane.gd
@@ -1,6 +1,10 @@
 extends CharacterBody2D
 
+# Getting the built in gravity value from project settings
 var _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
+
+# Constant for jump velocity
+const JUMP_POWER: float = -350.0
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -11,7 +15,12 @@ func _ready() -> void:
 func _process(delta: float) -> void:
 	pass
 
+# Use this function anytime you make any physics related changes
 func _physics_process(delta: float) -> void:
 	velocity.y += _gravity * delta
 	
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_POWER # Change velocity when space is pressed to make the plane jump
+	
+	# This moves the characterbody2d node using the velocity
 	move_and_slide()
