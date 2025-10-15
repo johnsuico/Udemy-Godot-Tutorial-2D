7312134b4acd9d9885f99558e3a4cba9596d2c7e
John Suico <johncdsuico@gmail.com>
2025-10-14 19:38:37 -0700

    Add laser and pipe movement

---
+++ tappy-plane-tutorial/Scenes/Pipes/Pipes.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+const SCROLL_SPEED: float = 120.0
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	pass # Replace with function body.
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	position.x -= SCROLL_SPEED * delta
+
+func _on_visible_on_screen_notifier_2d_screen_exited() -> void:
+	set_process(false)
+	queue_free()
