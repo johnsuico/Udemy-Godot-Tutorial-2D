d29c700bedc0d4cca63ccdb658d6d700691d0a8d
John Suico <johncdsuico@gmail.com>
2025-10-14 19:10:00 -0700

    Add die logic

--- tappy-plane-tutorial/Scenes/Plane/Plane.gd
+++ tappy-plane-tutorial/Scenes/Plane/Plane.gd
@@ -6,6 +6,9 @@ var _gravity: float = ProjectSettings.get("physics/2d/default_gravity")
 # Get the animation player. Using it for the jump animation
 @onready var animation_player: AnimationPlayer = $AnimationPlayer
 
+# Reference the animated_sprite_2d
+@onready var animated_sprite_2d: AnimatedSprite2D = $AnimatedSprite2D
+
 # Constant for jump velocity
 const JUMP_POWER: float = -350.0
 
@@ -20,12 +23,20 @@ func _process(delta: float) -> void:
 
 # Use this function anytime you make any physics related changes
 func _physics_process(delta: float) -> void:
-	velocity.y += _gravity * delta
-	
-	if Input.is_action_just_pressed("jump"):
-		velocity.y = JUMP_POWER # Change velocity when space is pressed to make the plane jump
-		animation_player.play("Jump")
-		
+	fly(delta)
 	
 	# This moves the characterbody2d node using the velocity
 	move_and_slide()
+	
+	if is_on_floor():
+		die()
+
+func fly(delta: float) -> void:
+	velocity.y += _gravity * delta
+	if Input.is_action_just_pressed("jump"):
+		velocity.y = JUMP_POWER # Change velocity when space is pressed to make the plane jump
+		animation_player.play("Jump")
+
+func die() -> void:
+	animated_sprite_2d.stop()
+	set_physics_process(false)
