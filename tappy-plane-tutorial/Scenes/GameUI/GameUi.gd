e6deebdc97ca15b47d7cb5c60ca83e8be1f8ac6d
John Suico <johncdsuico@gmail.com>
2025-10-22 21:12:28 -0700

    Integrate gamemanager global + gameover UI

--- tappy-plane-tutorial/Scenes/GameUI/GameUi.gd
+++ tappy-plane-tutorial/Scenes/GameUI/GameUi.gd
@@ -1,18 +1,27 @@
 extends Control
 
-var MAIN = load("uid://cmpyma78daeru")
+#var MAIN = load("uid://cmpyma78daeru") # Commented out as no longer needed, it's in GameManager global script now
 
+@onready var game_over_label: Label = $GameOverLabel
+@onready var press_space_label: Label = $PressSpaceLabel
 
+@onready var timer: Timer = $Timer
 
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("Exit"):                     
-		get_tree().change_scene_to_packed(MAIN)
+		#get_tree().change_scene_to_packed(MAIN) # Moved to GameManager global script
+		GameManager.load_main_scene()
+	elif press_space_label.visible == true and event.is_action_pressed("jump"):
+		GameManager.load_main_scene()
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+func _enter_tree() -> void:
+	SignalHub.on_plane_died.connect(on_plane_died)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	pass
+func on_plane_died() -> void:
+	game_over_label.show()
+	timer.start()
+
+func _on_timer_timeout() -> void:
+	game_over_label.hide()
+	press_space_label.show()
--- tappy-plane-tutorial/Scenes/Main/Main.gd
+++ tappy-plane-tutorial/Scenes/Main/Main.gd
@@ -1,11 +1,12 @@
 extends Control
 
-const GAME = preload("uid://c1c81lbj2vx5p")
+#const GAME = preload("uid://c1c81lbj2vx5p") # Commented out because using GameManager global script to load
 
 # Space bar will take you to the "game" scene
 func _unhandled_input(event: InputEvent) -> void:
 	if event.is_action_pressed("jump"):                     
-		get_tree().change_scene_to_packed(GAME)
+		#get_tree().change_scene_to_packed(GAME) # Moved to GameManager global script
+		GameManager.load_game_scene()
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
