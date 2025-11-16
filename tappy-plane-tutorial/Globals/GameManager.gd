bcf484929e7c060e4bb262e5a859db4f144cc4e0
John Suico <johncdsuico@gmail.com>
2025-11-16 11:54:03 -0800

    Add simple transition

--- tappy-plane-tutorial/Globals/GameManager.gd
+++ tappy-plane-tutorial/Globals/GameManager.gd
@@ -2,10 +2,14 @@ extends Node
 
 const GAME = preload("uid://c1c81lbj2vx5p")
 const MAIN = preload("uid://cmpyma78daeru")
+const SIMPLE_TRANSITION = preload("uid://bpjp5rhipi6uy")
 
+var next_scene: PackedScene
 
 func load_game_scene() -> void:
-	get_tree().change_scene_to_packed(GAME)
+	next_scene = GAME
+	get_tree().change_scene_to_packed(SIMPLE_TRANSITION)
 
 func load_main_scene() -> void:
-	get_tree().change_scene_to_packed(MAIN)
+	next_scene = MAIN
+	get_tree().change_scene_to_packed(SIMPLE_TRANSITION)
---
+++ tappy-plane-tutorial/Scenes/SimpleTransition/SimpleTransition.gd
@@ -0,0 +1,6 @@
+extends Control
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	await get_tree().create_timer(0.1).timeout
+	get_tree().change_scene_to_packed(GameManager.next_scene)
